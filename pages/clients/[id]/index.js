1ba4cb3d56cff1fab2259a8cacea1611ed817ee0
ShrivatsaBhatP <shrivatsa@robosoftin.com>
2021-03-15 17:14:25 +0530

    navigate programatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectPage() {
+  const router = useRouter();
+  function loadProjectHandler() {
+    // ...load data
+    // router.push("/clients/vatsa/projecta");
+    // router.replace("/clients/vatsa/projecta");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "vatsa", clientprojectid: "projecta" },
+    });
+  }
   return (
     <div>
       <h1>The Client Project Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
