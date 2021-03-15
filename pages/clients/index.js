cc44bed8ade691c68213e2322d34778126b3aacf
ShrivatsaBhatP <shrivatsa@robosoftin.com>
2021-03-15 17:05:42 +0530

    navigate to the dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function ClientPage() {
+  const clients = [
+    { id: "vatsa", name: "Shrivatsa" },
+    { id: "rayan", name: "Amith Rayan" },
+  ];
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
